1e0cad4cdd587432a06aca40bd48c12baa094b6b
jitendrareddy05 <86464742+jitendrareddy05@users.noreply.github.com>
2022-11-30 15:04:24 +0530

    Update fire-flink.py

--- fire-flink.py
+++ fire-flink.py
@@ -19,7 +19,7 @@ def login(token):
     }
     head["Authorization"] = "Bearer " + token
     suiteid = 'SUITE1001'
-    baseUrl = 'https://FIRE_FLINK_ENVIRONMENT.fireflink.com'
+    baseUrl = 'https://backend.fireflink.com'
 
     run = s.get(baseUrl + ':8102/optimize/v1/suite/runSetting/run/' + suiteid, headers=head)
     runout = json.loads(run.content)
